b6acd5c7aa2413df192afb047147fefcd48eb234
mangdagold <podchara.t@gmail.com>
2016-12-02 17:36:15 +0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>7Amulet.com</title>
+
+        <!-- Bootstrap CSS Version 3.3.7 -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Style CSS-->
+        <link href="/css/style.css" rel="stylesheet">
+        <!-- Carousel -->
+        <link href="/css/carousel.css" rel="stylesheet">
+        <!-- Sticky-footer styles for this template -->
+        <link href="/css/sticky-footer-navbar.css" rel="stylesheet">
+        <!-- Dashboard styles for this template -->
+        <link href="/css/dashboard.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <div id="fb-root"></div>
+<!-- Script for facebook -->
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+          fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+    </head>
+  
+<!-- Body part -->
+    <body>
+    
+    <!-- Top Nav Bar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar_signin" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">7AMULET</a>
+          </div>
+          <div id="navbar_signin" class="navbar-collapse collapse">
+            <form class="navbar-form navbar-right" method="post">
+              <div class="form-group">
+                <input type="text" placeholder="Email" class="form-control" name="email">
+              </div>
+              <div class="form-group">
+                <input type="password" placeholder="Password" class="form-control" name="password">
+              </div>
+              <button type="submit" class="btn btn-success" id="signin" name="signin"><span class="glyphicon glyphicon-log-in"> เข้าระบบ</span></button>
+              <a href="signup.php" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-user"> สมัครสมาชิก</span></a>
+              
+              <div class="form-group">
+              <div class="checkbox">
+                <label ><input type="checkbox"> Remember me</label>
+              </div>
+                <a href="#" class=""><p> ลืมรหัสผ่าน</p></a>
+              </div>
+            </form>
+          </div><!--/.navbar-collapse -->
+        </div>
+      </nav>
+    <!-- End Top Nav Bar -->
+
+
+
+
+    <!-- Start Blog -->
+    <div class="contrainer padding-top-30px">
+        <div class="row" >
+
+        <!-- Catagories -->
+            <div class="col-xs-2 col-md-2 show-border catagorie">.col-xs-2 .col-md-2
+                <div class="contrainer">
+                    <div class="row">
+                        <div class="col-xs-4 col-md-4">
+                            <div><img class="img-rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="50" height="50"></div>
+                        </div>
+                        <div class="col-xs-8 col-md-8">
+                            <div><a href="#">รายการส่วนตัว</a></div>
+                            <div><a href="#">แก้ไขข้อมูล</a></div>
+                            <div><a href="#">ออกจากระบบ</a></div>
+                        </div>
+                        
+                    </div>
+                </div>
+ 
+                
+                <hr>
+                <p>หมวดพระเครื่อง</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">พระเครื่องกรุงเทพ และปริมณฑล</a></li>
+                  <li><a href="#">พระเกจิสายภาคกลาง</a></li>
+                  <li><a href="#">พระเกจิสายตะวันออก</a></li>
+                  <li><a href="#">พระเกจิสายใต้</a></li>
+                  <li><a href="#">พระเกจิสายอีสาน</a></li>
+                  <li><a href="#">พระเกจิสายภาคเหนือ</a></li>
+                </ol>
+                <p>หมวดพระกรุ</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">พระกรุเนื้อดินเนื้อผง</a></li>
+                  <li><a href="#">พระกรุเนื้อชิน</a></li>
+                  <li><a href="#">พระกรุเนื้อโลหะสัมริด</a></li>
+                </ol>
+                <p>หมวดพระปิดตาเกจิจารย์</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">พระปิดตาเนื้อโลหะ</a></li>
+                  <li><a href="#">พระปิดตาไม่ใช่เนื้อโลหะ</a></li>
+                </ol>
+                <p>หมวดพระไม่รู้ที่</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">พระปิดตาไม่รู้ที่</a></li>
+                  <li><a href="#">พระกริ่งพระรูปเหมือนไม่รู้ที่</a></li>
+                  <li><a href="#">เหรียญเกจิอาจารย์ไม่รู้ที่</a></li>
+                  <li><a href="#">พระกรุไม่รู้ที่</a></li>
+                </ol>
+                <p>หมวดพระบูชา</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">พระบูชาเก่ายุคสมัยต่างๆ</a></li>
+                  <li><a href="#">พระบูชาและพระเกจิอาจารย์</a></li>
+                </ol>
+                <p>อุปกรณ์พระเครื่อง</p>
+                <ol class="list-unstyled">
+                  <li><a href="#">ตลับใส่พระและสร้อยคอ</a></li>
+                  <li><a href="#">กล้องส่องพระและอุปกรณ์ถ่ายภาพ</a></li>
+                  <li><a href="#">หนังสือพระ</a></li>
+                </ol>
+
+            </div>
+        <!-- End Catagories -->
+
+
+
+        <!-- Feeds -->
+            <div class="col-xs-5 col-md-5 show-border">.col-xs-5 .col-md-5
+
+            </div>
+        <!-- End Feeds -->
+
+
+
+        <!-- Advertise -->
+            <div class="col-xs-3 col-md-3 show-border">.col-xs-3 .col-md-3
+
+            </div>
+
+        <!-- End Advertise -->
+
+        <!-- Recent and Favorite view -->
+            <div class="col-xs-2 col-md-2 show-border">.col-xs-2 .col-md-2
+
+            </div>
+        <!-- End Recent and Favorite view -->
+
+
+
+        </div>
+    </div>
+    <!-- End Blog -->
+
+
+
+
+
+
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="container">
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2016 7amulet, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </div>
+    </footer>
+    <!-- End Footer -->
+
+
+
+
+
+<!-- JavaScripts -->
+        <!-- jQuery -->
+        <script src="/js/jquery-3.1.1.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/holder.min.js"></script>
+    </body>
+<!-- End Body part -->
+</html>
